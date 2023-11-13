b61bf532626933a0950112462bef964dada22bc0
Gustavo De Almeida Bassanetto <gustavo.almeidabassanetto@gmail.com>
2023-11-13 18:47:27 -0300

    Programação Web :: Tarefa 6 - Angular e Typescript

--- ac2/AngularTS/src/app/app.module.ts
+++ ac2/AngularTS/src/app/app.module.ts
@@ -6,18 +6,26 @@ import { AppComponent } from './app.component';
 import { CalculadoraMediaComponent } from './calculadora-media/calculadora-media.component';
 import { ListarComponentesComponent } from './listar-componentes/listar-componentes.component';
 import {CardModule} from "primeng/card";
+import {FormsModule} from "@angular/forms";
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { CadastroAlunoComponent } from './cadastro-aluno/cadastro-aluno.component';
+import { ApoliceSeguroComponent } from './apolice-seguro/apolice-seguro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculadoraMediaComponent,
-    ListarComponentesComponent
+    ListarComponentesComponent,
+    CalculadoraComponent,
+    CadastroAlunoComponent,
+    ApoliceSeguroComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CardModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        CardModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
